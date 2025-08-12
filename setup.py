78d77311a82de043e5807ab8162718faeb7b2b10
Welt Xing <62822155+WeltXing@users.noreply.github.com>
2025-08-12 23:18:15 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@ import setuptools
 
 setuptools.setup(
     name='pydynet',
-    version='0.1',
+    version='1.1',
     description=
     'PyDyNet: Neuron Network (MLP, CNN, RNN, Transformer, ...) implementation using Numpy with Autodiff',
-    author="Welt Xing",
-    author_email="xingcy@smail.nju.edu.cn",
-    maintainer="Welt Xing",
-    maintainer_email="xingcy@smail.nju.edu.cn",
+    author="Cun-Yuan Xing",
+    author_email="xingcy@lamda.nju.edu.cn",
+    maintainer="Cun-Yuan Xing",
+    maintainer_email="xingcy@lamad.nju.edu.cn",
     packages=['pydynet', 'pydynet/optim', 'pydynet/nn', 'pydynet/nn/modules'],
     license='MIT License',
     install_requires=['numpy'],
     long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type="text/markdown",
-    url='https://github.com/Kaslanarian/PyDyNet',
+    url='https://github.com/WeltXing/PyDyNet',
 )
